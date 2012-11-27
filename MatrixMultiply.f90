836a178f275ed8c7a2569e7c9cd91f17274cbff6
Samuel Shadwell <see@home2.ccs.ornl.gov>
2012-11-27 15:07:53 -0500

    Debugged with Arnold Tharrington, 11/19/2012

--- MatrixMultiply.f90
+++ MatrixMultiply.f90
@@ -6,22 +6,46 @@
 
 
 PROGRAM MatrixMultiply
-IMPLICIT NONE
+implicit none 
 INCLUDE 'mpif.h'
 
 !Variable declarations
-INTEGER                                 :: ierr, comm, commSize, rank           !MPI variables
+INTEGER                                 :: ierr, commSize, rank                 !MPI variables
 REAL, ALLOCATABLE, DIMENSION(:,:)       :: arayA, arayB, arayC                  !Matrices
-INTEGER                                 :: arayLen
-REAL                                    :: nodeAVal, nodeBVal
+INTEGER                                 :: arayLen, nodeRow, nodeCol
+REAL                                    :: nodeAVal, nodeBVal, workA, workB
+REAL                                    :: workC
 
 !Initialize MPI Environment
 CALL MPI_INIT(ierr)
-CALL MPI_COMM_SIZE (comm, commSize, ierr)
-CALL MPI_COMM_RANK (comm, rank, ierr)
+CALL MPI_COMM_SIZE (MPI_COMM_WORLD, commSize, ierr)
+CALL MPI_COMM_RANK (MPI_COMM_WORLD, rank, ierr)
+
+!Allocates arrays
+IF (rank .EQ. 0) THEN
+
+    arayLen = 1
+    ALLOCATE(arayA(arayLen, arayLen))
+    ALLOCATE(arayB(arayLen, arayLen))
+    ALLOCATE(arayC(arayLen, arayLen))
+
+ELSE
+
+    ALLOCATE(arayA(1,1))
+    ALLOCATE(arayB(1,1))
+    ALLOCATE(arayC(1,1))
+
+ENDIF
 
 !Sets and distributes array values to various nodes
-CALL setUpArrays(ierr, comm, commSize, rank, arayA, arayB, arayC, nodeAVal, nodeBVal, arayLen)
+CALL setUpArrays(ierr, commSize, rank, arayA, arayB, arayC, nodeAVal, nodeBVal, workA, workB, workC, arayLen)
+
+!TODO: Write this code
+!Passes values
+!CALL passValues(?)
+
+!Performs calculations
+!CALL performCalculations(arayA, arayB, arayC, rank)
 
 !Parallel Code Ends
 CALL MPI_FINALIZE (ierr)
@@ -37,22 +61,20 @@ END PROGRAM MatrixMultiply
 !   Puts entire array on head node (0), and distributes single elements to 
 !   all other nodes. Also sets size of arayA, B, and C to 1 for all
 !   MPI processes other than the head node.
-SUBROUTINE setUpArrays(ierr, comm, commSize, rank, arayA, arayB, arayC, nodeAVal, nodeBVal, arayLen)
-IMPLICIT NONE
-INTEGER                                 :: ierr, comm, commSize, rank, arayLen, mpiStatus
-REAL, ALLOCATABLE, DIMENSION(:,:)       :: arayA, arayB, arayC
-REAL                                    :: nodeAVal, nodeBVal
-INTEGER                                 :: i, j, n, tag
-
+SUBROUTINE setUpArrays(ierr, commSize, rank, arayA, arayB, arayC, nodeAVal, nodeBVal, workA, workB, workC, arayLen)
+implicit none 
+include 'mpif.h'
+INTEGER                                             :: counter,ierr, commSize, rank, arayLen, mpiStatus(MPI_STATUS_SIZE)
+REAL, DIMENSION(arayLen,arayLen)                    :: arayA, arayB, arayC
+REAL                                                :: nodeAVal, nodeBVal
+REAL, INTENT(OUT)                                   :: workA, workB, workC
+INTEGER                                             :: i, j, n, tag, nodeRow, nodeCol
+REAL aOut, bOut
 
 !Currently sets values for arrays, I need to figure out how to read them in
 
-arayLen = 3                 ! Array dimensions are 3x3
-
 IF (rank .EQ. 0) THEN       ! Begin head node
 
-    ALLOCATE(arayA(arayLen, arayLen), arayB(arayLen, arayLen), arayC(arayLen,arayLen))
-    
     !Populates arrays A and B
     !TODO: replace this with an actual parser
     DO i = 1, arayLen
@@ -64,27 +86,40 @@ IF (rank .EQ. 0) THEN       ! Begin head node
     ENDDO
 
     !Sends all nodes their values
+    destinationRank = 0
     n = arayLen**2          !Number of data values which must be sent
-    DO i = 1, n
-        CALL MPI_SEND(arayA(MODULO(i, arayLen), i/arayLen), 1, MPI_REAL, MODULO(i, commSize-1), 1, comm, ierr)
-        CALL MPI_SEND(arayB(MODULO(i, arayLen), i/arayLen), 1, MPI_REAL, MODULO(i, commSize-1), 2, comm, ierr)
+    DO i = 1, arayLen
+        DO j = 1, arayLen
+            aOut = arayA(i,j)
+            bOut = arayB(i,j)
+            CALL MPI_SEND(aOut, 1, MPI_REAL, destinationRank, 1, MPI_COMM_WORLD, ierr)
+           ! CALL MPI_SEND(bOut, 1, MPI_REAL, destinationRank, 2, MPI_COMM_WORLD, ierr)
+            destinationRank = destinationRank + 1
+        ENDDO
     ENDDO
 
 ENDIF                       ! End head node
 
 
-IF (rank .NE. 0) THEN       ! Nodes other than head node
-
-    ALLOCATE(arayA(1,1), arayB(1,1), arayC(1,1))
-    !Sets C array to 0
-    arayC(1,1) = 0.0
-    !Gets value for arrays A and B sent from head node
-    mpiStatus = status(MPI_STATUS_SIZE)
-
-    CALL MPI_RECV(nodeAVal, 1, MPI_REAL, 0, 1, comm, mpiStatus, ierr)
-    CALL MPI_RECV(nodeBVal, 1, MPI_REAL, 0, 2, comm, mpiStatus, ierr)
+!Gets value for arrays A and B sent from head node
 
 
-ENDIF                       ! End other nodes
+CALL MPI_RECV(nodeAVal, 1, MPI_REAL, 0, 1, MPI_COMM_WORLD, mpiStatus, ierr)
+!CALL MPI_RECV(nodeBVal, 1, MPI_REAL, 0, 2, MPI_COMM_WORLD, mpiStatus, ierr)
 
 END SUBROUTINE setUpArrays
+
+
+
+SUBROUTINE performCalculations (arayA, arayB, arayC, rank)
+
+REAL, ALLOCATABLE, DIMENSION(:,:)       :: arayA, arayB, arayC
+INTEGER                                 :: rank
+
+IF(rank .NE. 0) THEN
+    
+    !!arayC = arayC + (arayB*arayA)
+
+ENDIF
+
+END SUBROUTINE performCalculations
