ff63295ccd7e2d031330884d9a3bf80e0600fdda
Samuel Shadwell <smshdwll@gmail.com>
2012-11-30 13:50:31 -0500

    After initial debugging with Arnold. Still not functioning.

--- MatrixMultiply.f90
+++ MatrixMultiply.f90
@@ -1,4 +1,4 @@
-!Very rudimentary Fox Matrix Multiplication program
+!Very  rudimentary Fox Matrix Multiplication program
 !@author Samuel Shadwell
 !
 !KNOWN ISSUES: Only works with 3x3 matrices
@@ -10,43 +10,40 @@ IMPLICIT NONE
 INCLUDE 'mpif.h'
 
 !Variable declarations
-INTEGER                                 :: ierr, commSize, rank, r1, r2, r3, c1, c2, c3 !MPI variables
-REAL, ALLOCATABLE, DIMENSION(:,:)       :: arayA, arayB, arayC                          !Matrices
-INTEGER                                 :: arayLen, nodeRow, nodeCol
-REAL                                    :: nodeAVal, nodeBVal, workA, workB
-REAL                                    :: workC
-INTEGER, PARAMETER                      :: n = 3 !Size of matrices to be multiplied (n by n)
+INTEGER                                 :: ierr, commSize, worldRank
+INTEGER                                 :: r1Comm, r2Comm, r3Comm, c1Comm, c2Comm, c3Comm !MPI variables
+REAL, ALLOCATABLE, DIMENSION(:,:)       :: arayA, arayB, arayC                            !Matrices
+REAL                                    :: nodeAVal, nodeBVal, workA, workB, workC
+INTEGER, PARAMETER                      :: arayLen = 3 !Size of matrices to be multiplied (arayLen x arayLen)
 
 !Initialize MPI Environment
 CALL MPI_INIT(ierr)
 CALL MPI_COMM_SIZE (MPI_COMM_WORLD, commSize, ierr)
-CALL MPI_COMM_RANK (MPI_COMM_WORLD, rank, ierr)
+CALL MPI_COMM_RANK (MPI_COMM_WORLD, worldRank, ierr)
 
 !Allocates arrays
-IF (rank .EQ. 0) THEN
+IF (worldRank .EQ. 0) THEN
 
-    arayLen = n
     ALLOCATE(arayA(arayLen, arayLen))
     ALLOCATE(arayB(arayLen, arayLen))
     ALLOCATE(arayC(arayLen, arayLen))
 
-ELSE
-
-    ALLOCATE(arayA(1,1))
-    ALLOCATE(arayB(1,1))
-    ALLOCATE(arayC(1,1))
+!ELSE
+!
+!    ALLOCATE(arayA(1,1))
+!    ALLOCATE(arayB(1,1))
+!    ALLOCATE(arayC(1,1))
 
 ENDIF
 
 !Sets and distributes array values to various nodes
-CALL setUpArrays(ierr, commSize, rank, arayA, arayB, arayC, nodeAVal, nodeBVal, workA, workB, workC, arayLen)
+CALL setUpArrays(ierr, commSize, worldRank, arayA, arayB, arayC, nodeAVal, nodeBVal, arayLen)
 
-!TODO: Write this code
-!Passes values
-!CALL passValues(?)
+!Creates communicators and groups for matrix rows and columns
+CALL establishGroups(ierr, arayLen, r1Comm, r2Comm, r3Comm, c1Comm, c2Comm, c3Comm)
 
 !Performs calculations
-!CALL performCalculations(arayA, arayB, arayC, rank)
+CALL performCalculations(ierr, workA, workB, workC, arayLen, r1Comm, r2Comm, r3Comm, c1Comm, c2Comm, c3Comm)
 
 !Parallel Code Ends
 CALL MPI_FINALIZE (ierr)
@@ -57,7 +54,6 @@ CALL MPI_FINALIZE (ierr)
 END PROGRAM MatrixMultiply
 
 
-
 !============== SUBROUTINE establishGroups ==========
 !   Creates MPI groups to allow broadcasting of 
 !	matrix values across the appropriate rows
@@ -66,44 +62,54 @@ END PROGRAM MatrixMultiply
 !   NOTE: THIS IS NOT MODULAR DUE TO LIMITATIONS OF
 !   MYSELF AND FORTRAN
 !====================================================
-SUBROUTINE establishGroups(ierr, n, r1Grp, r2Grp, r3Grp, c1Grp, c2Grp, c3Grp)
+SUBROUTINE establishGroups(ierr, arayLen, r1Comm, r2Comm, r3Comm, c1Comm, c2Comm, c3Comm)
 IMPLICIT NONE
 INCLUDE 'mpif.h'
 
-INTEGER, INTENT(OUT)                               :: r1Grp, r2Grp, r3Grp
-INTEGER, INTENT(OUT)                               :: c1Grp, c2Grp, c3Grp !Groups to be established
-INTEGER, INTENT(IN)                                :: n
+INTEGER, INTENT(OUT)                               :: r1Comm, r2Comm, r3Comm   !Row communicators
+INTEGER, INTENT(OUT)                               :: c1Comm, c2Comm, c3Comm   !Column communicators
+
+INTEGER                                            :: r1Grp, r2Grp, r3Grp      !Row groups
+INTEGER                                            :: c1Grp, c2Grp, c3Grp      !Column groups
+
+INTEGER, INTENT(IN)                                :: arayLen
 INTEGER, DIMENSION(3)                              :: r1Ranks, r2Ranks, r3Ranks
 INTEGER, DIMENSION(3)                              :: c1Ranks, c2Ranks, c3Ranks
 INTEGER                                            :: wGroup, i, ierr
 
 CALL MPI_Comm_group(MPI_COMM_WORLD, wGroup)
 
-!Establishes which ranks belong to each row and column, placing the
+!Establishes which worldRanks belong to each row and column, placing the
 ! indeces into *Ranks arrays
-DO i = 0, n-1
+DO i = 0, arayLen-1
 
     r1Ranks(i+1) = i
-    r2Ranks(i+1) = i + n
-    r2Ranks(i+1) = i + 2*n
+    r2Ranks(i+1) = i + arayLen
+    r2Ranks(i+1) = i + 2*arayLen
 
-    c1Ranks(i+1) = i*n
-    c2Ranks(i+1) = i*n + 1
-    c3Ranks(i+1) = i*n + 2
+    c1Ranks(i+1) = i*arayLen
+    c2Ranks(i+1) = i*arayLen + 1
+    c3Ranks(i+1) = i*arayLen + 2
 
 ENDDO
 
-!Creates 6 (n*2) groups from the given ranks
-CALL MPI_Group_incl(wGroup, n, r1Ranks, r1Grp, ierr)
-CALL MPI_Group_incl(wGroup, n, r2Ranks, r2Grp, ierr)
-CALL MPI_Group_incl(wGroup, n, r3Ranks, r3Grp, ierr)
-CALL MPI_Group_incl(wGroup, n, c1Ranks, c1Grp, ierr)
-CALL MPI_Group_incl(wGroup, n, c2Ranks, c2Grp, ierr)
-CALL MPI_Group_incl(wGroup, n, c3Ranks, c3Grp, ierr) 
+!Creates 6 (arayLen*2) groups from the given worldRanks
+CALL MPI_Group_incl(wGroup, arayLen, r1Ranks, r1Grp, ierr)
+CALL MPI_Group_incl(wGroup, arayLen, r2Ranks, r2Grp, ierr)
+CALL MPI_Group_incl(wGroup, arayLen, r3Ranks, r3Grp, ierr)
+CALL MPI_Group_incl(wGroup, arayLen, c1Ranks, c1Grp, ierr)
+CALL MPI_Group_incl(wGroup, arayLen, c2Ranks, c2Grp, ierr)
+CALL MPI_Group_incl(wGroup, arayLen, c3Ranks, c3Grp, ierr) 
 
+!Creates communicators from the given groups
+CALL MPI_Comm_create(MPI_COMM_WORLD, r1Grp, r1Comm , ierr)
+CALL MPI_Comm_create(MPI_COMM_WORLD, r2Grp, r2Comm , ierr)
+CALL MPI_Comm_create(MPI_COMM_WORLD, r3Grp, r3Comm , ierr)
+CALL MPI_Comm_create(MPI_COMM_WORLD, c1Grp, c1Comm , ierr)
+CALL MPI_Comm_create(MPI_COMM_WORLD, c2Grp, c2Comm , ierr)
+CALL MPI_Comm_create(MPI_COMM_WORLD, c3Grp, c3Comm , ierr)
 
 END SUBROUTINE establishGroups
-
 
 
 !============== SUBROUTINE SetUpArrays ===============
@@ -111,22 +117,21 @@ END SUBROUTINE establishGroups
 !   all other nodes. Also sets size of arayA, B, and C to 1 for all
 !   MPI processes other than the head node.
 !====================================================
-SUBROUTINE setUpArrays(ierr, commSize, rank, arayA, arayB, arayC, nodeAVal, nodeBVal, workA, workB, workC, arayLen)
+SUBROUTINE setUpArrays(ierr, commSize, worldRank, arayA, arayB, arayC, nodeAVal, nodeBVal, arayLen)
 IMPLICIT NONE 
 INCLUDE 'mpif.h'
-INTEGER                                             :: counter,ierr, commSize, rank, arayLen, mpiStatus(MPI_STATUS_SIZE)
+
+INTEGER                                             :: counter, ierr, commSize, worldRank, arayLen, mpiStatus(MPI_STATUS_SIZE)
 REAL, DIMENSION(arayLen,arayLen)                    :: arayA, arayB, arayC
 REAL                                                :: nodeAVal, nodeBVal
-REAL, INTENT(OUT)                                   :: workA, workB, workC
-INTEGER                                             :: i, j, n, tag, nodeRow, nodeCol
+INTEGER                                             :: i, j, tag, nodeRow, nodeCol, destinationRank
 REAL aOut, bOut
 
 !Currently sets values for arrays, I need to figure out how to read them in
 
-IF (rank .EQ. 0) THEN       ! Begin head node
+IF (worldRank .EQ. 0) THEN       ! Begin head node
 
     !Populates arrays A and B
-    !TODO: replace this with an actual parser
     DO i = 1, arayLen
         DO j = 1, arayLen
             arayA(i,j) = i+j
@@ -137,13 +142,14 @@ IF (rank .EQ. 0) THEN       ! Begin head node
 
     !Sends all nodes their values
     destinationRank = 0
-    n = arayLen**2          !Number of data values which must be sent
     DO i = 1, arayLen
         DO j = 1, arayLen
-            aOut = arayA(i,j)
-            bOut = arayB(i,j)
-            CALL MPI_SEND(aOut, 1, MPI_REAL, destinationRank, 1, MPI_COMM_WORLD, ierr)
-           ! CALL MPI_SEND(bOut, 1, MPI_REAL, destinationRank, 2, MPI_COMM_WORLD, ierr)
+            IF (destinationRank .NE. 0) THEN
+                aOut = arayA(i,j)
+                bOut = arayB(i,j)
+                CALL MPI_SEND(aOut, 1, MPI_REAL, destinationRank, 1, MPI_COMM_WORLD, ierr)
+                CALL MPI_SEND(bOut, 1, MPI_REAL, destinationRank, 2, MPI_COMM_WORLD, ierr)
+            ENDIF            
             destinationRank = destinationRank + 1
         ENDDO
     ENDDO
@@ -152,28 +158,63 @@ ENDIF                       ! End head node
 
 
 !Gets value for arrays A and B sent from head node
-
-
-CALL MPI_RECV(nodeAVal, 1, MPI_REAL, 0, 1, MPI_COMM_WORLD, mpiStatus, ierr)
-!CALL MPI_RECV(nodeBVal, 1, MPI_REAL, 0, 2, MPI_COMM_WORLD, mpiStatus, ierr)
+IF (worldRank .NE. 0) THEN
+    CALL MPI_RECV(nodeAVal, 1, MPI_REAL, 0, 1, MPI_COMM_WORLD, mpiStatus, ierr)
+    CALL MPI_RECV(nodeBVal, 1, MPI_REAL, 0, 2, MPI_COMM_WORLD, mpiStatus, ierr)
+ENDIF
 
 END SUBROUTINE setUpArrays
 
 
-
 !========================SUBROUTINE performCalculations==========
+!   Subroutine which controls the broadcasting of values across
+!   nodes as well as the calculating of C value
+!================================================================
+SUBROUTINE performCalculations(ierr, workA, workB, workC, arayLen, r1Comm, r2Comm, r3Comm, c1Comm, c2Comm, c3Comm)
+INTEGER                                            :: arayLen, i, r1Comm, r2Comm, r3Comm, c1Comm, c2Comm, c3Comm, ierr
+REAL                                               :: workA, workB, workC
+
+workC = 0.0d0
+
+DO i = 0, arayLen-1
+
+    CALL passValues(i, workA, workB, workC, r1Comm, r2Comm, r3Comm, c1Comm, c2Comm, c3Comm, ierr)
+    CALL calculate(workA, workB, workC)
+
+ENDDO
+
+OPEN(UNIT = 11, FILE = "finalMatrix.txt")
+WRITE(11,*) "NODE: ", worldRank, "   VALUE: ", workC
+CLOSE(11)
+
+END SUBROUTINE performCalculations
+
+
+!========================SUBROUTINE calculate====================
 !   Relatively simple subroutine which performs the value addition
 !   and multiplication (C = C + AxB)
 !================================================================
-SUBROUTINE performCalculations (arayA, arayB, arayC, rank)
+SUBROUTINE calculate(workA, workB, workC)
+REAL                                                :: workA, workB, workC
 
-REAL, ALLOCATABLE, DIMENSION(:,:)       :: arayA, arayB, arayC
-INTEGER                                 :: rank
+workC = workC + (workB*workA)
 
-IF(rank .NE. 0) THEN
-    
-    !!arayC = arayC + (arayB*arayA)
+END SUBROUTINE calculate
 
-ENDIF
 
-END SUBROUTINE performCalculations
+!========================SUBROUTINE passValues===================
+!   Subroutine which passes the necessary values to the arrays
+!================================================================
+SUBROUTINE passValues(i, workA, workB, workC, r1Comm, r2Comm, r3Comm, c1Comm, c2Comm, c3Comm)
+INTEGER                                            :: i, r1Comm, r2Comm, r3Comm, c1Comm, c2Comm, c3Comm, ierr
+REAL                                               :: workA, workB, workC
+
+CALL MPI_Bcast(workA, 1, MPI_REAL, i, r1Commr, ierr)
+CALL MPI_Bcast(workA, 1, MPI_REAL, i, r2Commr, ierr)
+CALL MPI_Bcast(workA, 1, MPI_REAL, i, r3Commr, ierr)
+
+CALL MPI_Bcast(workB, 1, MPI_REAL, i, c1Commr, ierr)
+CALL MPI_Bcast(workB, 1, MPI_REAL, i, c2Commr, ierr)
+CALL MPI_Bcast(workB, 1, MPI_REAL, i, c3Commr, ierr)
+
+END SUBROUTINE passValues
